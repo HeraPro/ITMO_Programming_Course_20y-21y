2c4e22180d4cbd608cf4f7655faf931ef5e92964
Ovanes Khandzyan <khandzyan@gmail.com>
2021-05-22 10:52:33 +0300

    just another update

--- lab4/cmake-build-debug/CMakeFiles/lab4.dir/DependInfo.cmake
+++ lab4/cmake-build-debug/CMakeFiles/lab4.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/hera/CLionProjects/lab4/main.cpp" "/home/hera/CLionProjects/lab4/cmake-build-debug/CMakeFiles/lab4.dir/main.cpp.o"
+  "/home/hera/CLionProjects/ITMO_Programming_Course_20y-21y/lab4/main.cpp" "/home/hera/CLionProjects/ITMO_Programming_Course_20y-21y/lab4/cmake-build-debug/CMakeFiles/lab4.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
