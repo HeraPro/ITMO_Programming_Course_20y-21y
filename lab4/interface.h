1fe39e28ac146c3a5bddf6805eadf3d4a888fe61
Ovanes Khandzyan <khandzyan@gmail.com>
2021-05-30 09:36:23 +0300

    k

---
+++ lab4/interface.h
@@ -0,0 +1,130 @@
+#pragma once
+
+namespace myspace {
+
+    template <typename N>
+    bool more (N first, N second){
+        return second > first;
+    }
+
+    template <typename N>
+    bool positive_numbers (N x){
+        return x > 0;
+    }
+    template <typename N>
+    bool negative_numbers (N x){
+        return x < 0;
+    }
+
+    template <typename N>
+    bool equality (N first, N second){
+        return first == second;
+    }
+
+    template <typename N>
+    bool less (N first, N second){
+        return first < second;
+    }
+
+    template <typename N>
+    bool greater_or_eq (N first, N second){
+        return first >= second;
+    }
+
+    template <typename N>
+    bool less_or_eq (N first, N second){
+        return first <= second;
+    }
+
+
+    template <typename InputIterator, typename Function>
+    bool all_of(InputIterator begin, InputIterator end, Function func){
+        for (; begin != end; ++begin){
+            if (!func(*begin))
+                return false;
+        }
+        return true;
+    }
+
+    template <typename InputIterator, typename Function>
+    bool any_of(InputIterator begin, InputIterator end, Function func){
+        for (; begin != end; ++begin) {
+            if (func(*begin))
+                return true;
+        }
+        return false;
+    }
+
+    template <typename InputIterator, typename Function>
+    bool none_of(InputIterator begin, InputIterator end, Function func) {
+        return !myspace::any_of(begin, end, func);
+    }
+
+    template <typename InputIterator, typename Function>
+    bool one_of(InputIterator begin, InputIterator end, Function func){
+        for (bool exist = false; begin != end; ++begin){
+            if (func(*begin) && !exist)
+                exist = true;
+            else if (func(*begin) && exist)
+                return false;
+        }
+        return true;
+    }
+
+    template <typename InputIterator, typename Function>
+    bool is_sorted(InputIterator begin, InputIterator end, Function func){
+        for (InputIterator back = begin++; begin != end; ++begin, ++back) {
+            if (!func(*back, *begin))
+                return false;
+        }
+        return true;
+
+    }
+
+    template <typename InputIterator, typename Function>
+    bool is_partitioned(InputIterator begin, InputIterator end, Function func){
+        for (; begin != end; ++begin) {
+            if (!func(*begin))
+                break;
+        }
+        for (; begin != end; ++begin) {
+            if (func(*begin))
+                return false;
+        }
+        return true;
+    }
+
+    template <typename InputIterator, typename Element>
+    InputIterator find_not(InputIterator begin, InputIterator end, Element elem){
+        for (; begin != end; ++begin){
+            if (*begin != elem)
+                return begin;
+        }
+        return end;
+    }
+
+    template <typename BidirectedIterator, typename Element>
+    BidirectedIterator find_backward(BidirectedIterator begin, BidirectedIterator end, Element elem){
+        for (; end != begin;) {
+            --end;
+            if (*end == elem)
+                return end;
+        }
+        return begin;
+
+    }
+
+    template <typename RandomAccessIterator, typename Function> //ama right with naming iterators? check out later
+    bool is_palindrome(RandomAccessIterator begin, RandomAccessIterator end, Function func){
+        for (; begin != end; ){
+            --end;
+            if (begin == end) //have to work on moving iter with even and uneven combs
+                return true;
+            if (!func(*begin, *end))
+                return false;
+            ++begin;
+        }
+        return false;
+    }
+
+}
---
+++ lab4/main.cpp
@@ -0,0 +1,23 @@
+#include "interface.h"
+#include <iostream>
+#include <vector>
+
+using namespace myspace;
+
+int main()
+{
+    std::vector<int> vec = {1, 2, 3, 3, -1, 1};
+
+    std::cout << std::boolalpha;
+    std::cout << "all_of - " << myspace::all_of(vec.begin(), vec.end(), positive_numbers<int>) << '\n';
+    std::cout << "any_of - " << myspace::any_of(vec.begin(), vec.end(), positive_numbers<int>) << '\n';
+    std::cout << "none_of - " << myspace::none_of(vec.begin(), vec.end(), positive_numbers<int>) << '\n';
+    std::cout << "one_of - " << one_of(vec.begin(), vec.end(), positive_numbers<int>) << '\n';
+    std::cout << "is_sorted - " << myspace::is_sorted(vec.begin(), vec.end(), more<int>) << '\n';
+    std::cout << "is_partitioned - " << myspace::is_partitioned(vec.begin(), vec.end(), negative_numbers<int>) << '\n';
+    std::cout << "find_not - " << *find_not(vec.begin(), vec.end(), 1) << '\n';
+    std::cout << "find_backward - " << *find_backward(vec.begin(), vec.end(), 2) << '\n';
+    std::cout << "is_palindrome - " << is_palindrome(vec.begin(), vec.end(), equality<int>) << '\n';
+
+    return 0;
+}
